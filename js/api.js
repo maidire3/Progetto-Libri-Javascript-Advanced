643ea014e9c924aa497e4a76c350f97ff33d85d8
maidire3 <lookcart123@gmail.com>
2026-03-17 03:56:22 +0100

    Aggiunta Api.js con fetch al sito

---
+++ js/api.js
@@ -0,0 +1,33 @@
+const BaseUrl="https://openlibrary.org"
+
+// Filtra i libri per la categoria
+async function fetchCategoryBooks(category){
+    try{
+        const response= await fetch(`${BaseUrl}/subjects/${category}.json`)
+        if(!response.ok)
+            throw new Error("Errore durante il caricamento dei libri!")
+        const data=await response.json();
+        console.log(data.works);
+        return data.works || [];
+    }catch(error){
+        console.error(error)
+    }
+}
+
+//Cerca la descrizione dalla key ricevuta
+async function fetchDetailsBook(key){
+    try{
+        const response= await fetch(`${BaseUrl}${key}.json`)
+        if(!response.ok)
+            throw new Error("Errore durante il caricamento dei dettagli!");
+        const data= await response.json();
+        console.log(data.description);
+        return data.description || [];
+    }catch(error){
+        console.error(error)
+    }
+}
+
+//Esempi di Test
+/* fetchCategoryBooks("Adventure")
+fetchDetailsBook("/works/OL45089W") */
