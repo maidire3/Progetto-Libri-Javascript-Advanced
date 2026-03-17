c2cbcfbfac91805c615990bb44ef72fa2cecce2f
maidire3 <lookcart123@gmail.com>
2026-03-17 05:38:54 +0100

    Aggiunto caricamento e alert se non vengono trovati libri

--- js/script.js
+++ js/script.js
@@ -8,8 +8,18 @@ async function searchBooks() {
     const category = categoryInput.value.trim();
     if (!category) return alert("Inserisci una CATEGORIA!");
 
+    searchBtn.disabled = true;
+    searchBtn.textContent = "Caricamento...";
+    
     const books = await fetchCategoryBooks(category);
+    if (!books || books.length === 0) {
+        alert("Nessun libro trovato per questa categoria!");
+        return;
+    }
     viewBookList(books, clickedBook);
+    
+    searchBtn.disabled = false;
+    searchBtn.textContent = "Cerca";
 }
 
 async function clickedBook(key){
